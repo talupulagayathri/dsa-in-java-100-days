f1b0df1b158945e039f6c6678a7d09b76c0a3886
talupulagayathri <talupulagayathri@gmail.com>
2026-06-20 18:56:29 +0530

    Right angle triangle,square

--- Patterns.java
+++ Patterns.java
@@ -1,11 +1,24 @@
+// public class Patterns {
+//     public static void main(String[] args){
+//         int n=4;
+//         for(int i=0;i<n;i++){
+//             for(int j=0;j<=i;j++){
+//                 System.out.print("* ");
+//             }
+//             System.out.println();
+//         }
+//     }
+// }
+
+//square
 public class Patterns {
     public static void main(String[] args){
         int n=4;
         for(int i=0;i<n;i++){
-            for(int j=0;j<=i;j++){
+            for(int j=0;j<n;j++){
                 System.out.print("* ");
             }
-            System.out.println();
-        }
+        System.out.println();
     }
+}
 }
