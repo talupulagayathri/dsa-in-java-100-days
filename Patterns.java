163ac8fe30d77ad6f172f687904720156cc9e6fd
talupulagayathri <talupulagayathri@gmail.com>
2026-06-20 19:00:59 +0530

    Right Angle Triangle,square,reverse of aRight angle triangle

--- Patterns.java
+++ Patterns.java
@@ -11,14 +11,26 @@
 // }
 
 //square
-public class Patterns {
+// public class Patterns {
+//     public static void main(String[] args){
+//         int n=4;
+//         for(int i=0;i<n;i++){
+//             for(int j=0;j<n;j++){
+//                 System.out.print("* ");
+//             }
+//         System.out.println();
+//     }
+// }
+// }
+//reverse Right angle triangle
+public class Triangle {
     public static void main(String[] args){
         int n=4;
-        for(int i=0;i<n;i++){
-            for(int j=0;j<n;j++){
+        for(int i=n-1;i>=0;i--){
+            for(int j=0;j<=i;j++){
                 System.out.print("* ");
             }
-        System.out.println();
+            System.out.println();
+        }
     }
-}
 }
