ed75a59d58ed4fe7202fb188473a0246a97c194b
talupulagayathri <talupulagayathri@gmail.com>
2026-06-20 17:43:24 +0530

    sum of natural numbers

---
+++ Demo.java
@@ -0,0 +1,10 @@
+class Demo{
+    public static void main(String[] args){
+        int n=4;
+        int sum=0;
+        for(int i=1;i<=n;i++){
+            sum=sum+i;
+        }
+        System.out.println(sum);
+    }
+}
